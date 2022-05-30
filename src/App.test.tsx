a9dd8333dcd6c3b7bd93c18b7d9a4e1db090cb9b
husseinTalal2 <hossientalal2@gmail.com>
2022-05-30 16:22:46 +0300

    added integration tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,68 @@
 import React from 'react';
-import {render, screen} from '@testing-library/react';
+import {render, screen, fireEvent, cleanup, waitFor } from '@testing-library/react';
 import App from './App';
 import LocalStoragePersistenceAdapter from './services/localStoragePersistenceAdapter';
+import userEvent from '@testing-library/user-event';
+import idGeneratorAdapter from './services/idGeneratorAdapter';
 
-test('renders learn react link', () => {
-  render(<App persistence={new LocalStoragePersistenceAdapter()} idGen={() => 'id'} />);
+describe("<App /> is functioning well", () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
 
-  const linkElement = screen.getByText(/Midient/i);
+  test('the app renders', () => {
+    render(<App persistence={new LocalStoragePersistenceAdapter()} idGen={idGeneratorAdapter} />);
+  
+    const linkElement = screen.getByText(/Midient/i);
+  
+    expect(linkElement).toBeInTheDocument();
+  });
+  
+  test('user can add a todo', () => {
+    render(<App persistence={new LocalStoragePersistenceAdapter()} idGen={idGeneratorAdapter} />);
+    
+    fireEvent.click(screen.getByText(/Add/i));
+    const inputElement = screen.getByTestId("todo-input")
+    userEvent.type(inputElement, "test todo");
+    fireEvent.click(screen.getByTestId("todo-submit"));
 
-  expect(linkElement).toBeInTheDocument();
+    const todoText = screen.getByText(/test todo/i);
+  
+    expect(todoText).toBeInTheDocument();
+  });
+
+  test('user can see todos after refresh',async () => {
+    const persistence = new LocalStoragePersistenceAdapter();
+    const {rerender} = render(<App persistence={persistence} idGen={idGeneratorAdapter} />);
+
+    fireEvent.click(screen.getByText(/Add/i));
+    const inputElement = screen.getByTestId("todo-input")
+    userEvent.type(inputElement, "test todo");
+    fireEvent.click(screen.getByTestId("todo-submit"));
+
+    rerender(<App persistence={persistence} idGen={idGeneratorAdapter} />);
+    expect(() => screen.getByText(/test todo/i)).not.toThrow();
+  });
+  
+  test('user can delete todo',async () => {
+    const persistence = new LocalStoragePersistenceAdapter();
+    render(<App persistence={persistence} idGen={idGeneratorAdapter} />);
+
+    fireEvent.click(screen.getByText(/Add/i));
+    const inputElement = screen.getByTestId("todo-input")
+    userEvent.type(inputElement, "test todo");
+    fireEvent.click(screen.getByTestId("todo-submit"));
+    fireEvent.click(screen.getByTestId("delete-todo"));
+
+    expect(() => screen.getByText(/test todo/i)).toThrow()
+  });
+
+  test('User gets an onboarding todo on first open',async () => {
+    const persistence = new LocalStoragePersistenceAdapter();
+    render(<App persistence={persistence} idGen={idGeneratorAdapter} />);
+
+    const todoText = await waitFor(() => screen.getByText(/Welcome to todo-app/i));  
+    expect(todoText).toBeInTheDocument();
+  });
+
 });
